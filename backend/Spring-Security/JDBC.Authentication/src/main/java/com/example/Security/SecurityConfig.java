bcf99ef4604262ffdf71501e4f967f2d91a7244c
Youssef <youssefaboueljihade@gmail.com>
2025-02-11 23:53:37 +0100

    UPDATE JDBC AUTHENTIFICATION

--- backend/Spring-Security/JDBC.Authentication/src/main/java/com/example/Security/SecurityConfig.java
+++ backend/Spring-Security/JDBC.Authentication/src/main/java/com/example/Security/SecurityConfig.java
@@ -1,7 +1,45 @@
-package main.java.com.example.Security;
+package com.example.Security;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public JdbcUserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+        manager.setUsersByUsernameQuery("SELECT username , password , enabled from users where username = ?");
+        manager.setAuthoritiesByUsernameQuery("SELECT username, authority FROM authorities WHERE username = ?");
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean 
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+        .requestMatchers("/admin").hasRole("ADMIN")
+        .requestMatchers("/user").hasAnyRole("ADMIN" , "USER")
+        .anyRequest().authenticated())
+        .formLogin((form) -> form.permitAll())
+        .logout((logout) -> logout.permitAll()); 
+
+        return http.build();
+    }
 
 
 }
--- projects/sms/src/main/java/com/example/SecurityConfig.java
+++ projects/sms/src/main/java/com/example/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -59,7 +60,9 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // Use BCrypt for password encoding
+        return NoOpPasswordEncoder.getInstance(); // Use BCrypt for password encoding
     }
+
+
 
 }
