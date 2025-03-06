b84471baf84aabe2e66ff9c5fe1840bb0864d877
youssef88ab <youssefaboueljihade@gmail.com>
2025-03-06 23:22:17 +0000

    update

--- frontend/Angular/ecommerce-app/src/app/pages/contact-us/contact-us.component.ts
+++ frontend/Angular/ecommerce-app/src/app/pages/contact-us/contact-us.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
+  
 })
 export class ContactUsComponent {
 
